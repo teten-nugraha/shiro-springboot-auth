4fffce863e74c065cbdaaac60f12c70699d571f0
Teten Nugraha <teten.nugraha18@gmail.com>
2022-05-31 13:37:42 +0700

    first launch

---
+++ src/main/java/id/ten/shirospringbootauth/ShiroSpringbootAuthApplication.java
@@ -0,0 +1,13 @@
+package id.ten.shirospringbootauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroSpringbootAuthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShiroSpringbootAuthApplication.class, args);
+  }
+
+}
